c520dfc83cd41158d9d280074e66d6e3ea69f71f
UglukFearless <UglukFearless@mail.ru>
2021-04-05 03:06:00 +0700

    Add lesson about http requests

---
+++ 06-http/22-client/src/app/app.component.ts
@@ -0,0 +1,28 @@
+import { Component } from '@angular/core';
+import { HttpService } from './services/http.service';
+import { User } from './model/User';
+    
+@Component({
+    selector: 'my-app',
+    template: `
+    <div class="col-4">
+        <ul>
+            <li *ngFor="let user of users">
+                <p>Имя пользователя: {{ user?.name }}</p>
+                <p>Возраст пользователя: {{ user?.age }}</p>
+            </li>
+        </ul>
+    </div>
+    `,
+    providers: [HttpService]
+})
+export class AppComponent {
+
+    users: User[];
+
+    constructor(private httpService: HttpService) {}
+
+    ngOnInit() {
+        this.httpService.getData().subscribe((data) => this.users = data['userList']);
+    }
+}
